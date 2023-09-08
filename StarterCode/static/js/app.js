a067eabf4bda710ffb1846c676480c91566e99ae
M0412 <133132845+M0412@users.noreply.github.com>
2023-09-08 16:46:12 -0500

    bubble chart complete

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -37,6 +37,7 @@ function init() {
         // Call the functions to make the demographic panel, bar chart, and bubble chart
         demographic(first_sample);
         barchart(first_sample);
+        bubblechart(first_sample);
        
     });
 
@@ -116,12 +117,52 @@ function barchart(sample) {
 
 };
 
+// Function that builds the bubble chart
+function bubblechart(sample) {
+
+    // Fetch the JSON data
+    d3.json(url).then((data) => {
+
+        // Retrieve all sample data
+        let sampledata = data.samples;
+
+        // Filter based on the value of the sample
+        let filteredData = sampledata.filter((value) => value.id == sample);
+
+        // Get the first index from the array
+        let valueData = filteredData[0];
+
+        // Set up the trace for bubble chart
+        let trace = {
+            x: valueData.otu_ids,
+            y: valueData.sample_values,
+            text: valueData.sample_values,
+            mode: "markers",
+            marker: {
+                size: valueData.sample_values,
+                color: valueData.otu_ids,
+                colorscale: "Earth"
+            }
+        }
+
+        // Set up the layout the layout
+        let layout = {
+            xaxis: {title: "OTU ID"}
+        };
+
+        // Call Plotly to plot the bar chart
+        Plotly.newPlot("bubble", [trace], layout);
+
+    });
+};
+
 // Function that updates dashboard when sample is changed
 function optionChanged(value) { 
 
     // Call all functions 
     demographic(value);
     barchart(value);
+    bubblechart(value);
     
 };
 
