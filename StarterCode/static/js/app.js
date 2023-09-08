efe0c1c028b7ad0302219daa3697e77f35f3c522
M0412 <133132845+M0412@users.noreply.github.com>
2023-09-08 13:46:46 -0500

    demographic and barchart complete

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -19,14 +19,14 @@ function init() {
         let names = data.names;
 
         // Add  samples to dropdown menu
-        names.forEach((id) => {
+        for (let i = 0; i< names.length; i++){
             
             // Console log the value of id for each iteration of the loop
-            console.log(id);
+            console.log(names[i]);
             
-            dropdownMenu.append("option").text(id).property("value", id);
+            dropdownMenu.append("option").text(names[i]).property("value", names[i]);
         
-        });
+        };
         
         // Set the first sample from the list
         let first_sample = names[0];
@@ -37,9 +37,7 @@ function init() {
         // Call the functions to make the demographic panel, bar chart, and bubble chart
         demographic(first_sample);
         barchart(first_sample);
-        bubblechart(first_sample);
-        gauge(first_sample);
-
+       
     });
 
 };
@@ -60,21 +58,72 @@ function demographic(sample) {
         console.log(filteredData)
 
         // Get the first index from the array
-        let valueData = value[0];
+        let valueData = filteredData[0];
 
         // Clear the child elements in div 
         d3.select("#sample-metadata").html("");
 
         // Use Object.entries to add each key/value pair to the panel
-        Object.entries(valueData).forEach(([key,value]) => {
+        for (key in valueData){
 
             // Console log the individual key/value pairs 
-            console.log(key,value);
+            console.log(key,valueData[key]);
 
-            d3.select("#sample-metadata").append("h5").text(`${key}: ${value}`);
+            d3.select("#sample-metadata").append("h5").text(`${key.toUpperCase()}: ${valueData[key]}`);
         
-        });
+        };
 
     });
 
 };
+
+// Function that builds the bar chart
+function barchart(sample) {
+
+    // Fetch the JSON data
+    d3.json(url).then((data) => {
+       
+        // Retrieve all sample data
+        let sampledata = data.samples;
+
+        // Filter based on the value of the sample
+        let filteredData = sampledata.filter((value) => value.id == sample);
+
+        // Console log the array of the sample data that has been filtered
+        console.log(filteredData)
+
+        // Get the first index from the array
+        let valueData = filteredData[0];
+        
+        // Trace for the data for the horizontal bar chart
+        // Set top ten items to display in descending order
+        let yticks = valueData.otu_ids.slice(0,10).map((otu_id) => `OTU ${otu_id}`).reverse();
+        let xticks = valueData.sample_values.slice(0,10).reverse();
+        let labels = valueData.otu_labels.slice(0,10).reverse();
+        
+        // Set up the trace for the bar chart
+        let trace = {
+            x: xticks,
+            y: yticks,
+            text: labels,
+            type: "bar",
+            orientation: "h"
+        };
+
+        // Call Plotly to plot the bar chart
+        Plotly.newPlot("bar", [trace]);
+    });
+
+};
+
+// Function that updates dashboard when sample is changed
+function optionChanged(value) { 
+
+    // Call all functions 
+    demographic(value);
+    barchart(value);
+    
+};
+
+// Call the initialize function
+init();
